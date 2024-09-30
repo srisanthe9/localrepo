e34313032035321459506aa6d5ebc5bfbaab15d2
Sridhar Kondaboina <srisanth.e9@gmail.com>
2024-09-30 23:04:07 +0530

    adding changes

--- example.c
+++ example.c
@@ -1,1 +1,15 @@
-hello everyone
+#include<stdio.h>
+
+void main(){
+int n;
+printf("Enter the value of n:");
+scanf("%d",&n);
+
+for (int i=1;i<=n;i++)
+{if(i%2==0)printf("%3d",i);
+printf("even numbers are:");}
+
+for (int i=1;i<=n;i++)
+{if(i%2!=0)printf("%3d",i);
+printf("\nodd numbers are:");}
+}
